47c0edd738bc8e1655d8fed9155858e0d7f12a21
sohanur.rahman <sohanur.rahaman@btracsl.com>
2023-01-23 19:00:22 +0600

    Controller and Model Basic

---
+++ CoreMVCFirstApp/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using CoreMVCFirstApp.Models;
+using CoreMVCFirstApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCFirstApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookRepository = null;
+
+        public BookController() 
+        {
+            bookRepository= new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return bookRepository.GetAllBooks();
+        }
+        public BookModel GetBook(int id)
+        {
+            return bookRepository.GetBook(id);
+        }
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return bookRepository.SearchBooks(bookName, authorName);
+        }
+    }
+}
---
+++ CoreMVCFirstApp/Repository/BookRepository.cs
@@ -0,0 +1,32 @@
+﻿using CoreMVCFirstApp.Models;
+
+namespace CoreMVCFirstApp.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBook(int id)
+        {
+            return DataSource().Where(x=>x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBooks(string title, string authorName) 
+        {
+            return DataSource().Where(x=>x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
+
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() {Id=1, Title="C Programming", Author="sohan"},
+                new BookModel() {Id=2, Title="Data Structure", Author="afroja"},
+                new BookModel() {Id=3, Title="Algorithm", Author="sohan"},
+                new BookModel() {Id=4, Title="Database", Author="Saif"}
+            };
+        }
+    }
+}
